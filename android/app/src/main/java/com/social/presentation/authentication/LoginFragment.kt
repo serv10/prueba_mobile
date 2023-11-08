c9fd34fc03f374fcac4f507cfd0944bbc9d3593f
serv10 <sebastian.ramos.villanueva10@gmail.com>
2023-11-08 00:15:39 -0500

    Utilizando GOOGLE_APPLICATION_CREDENTIALS

--- android/app/src/main/java/com/social/presentation/authentication/LoginFragment.kt
+++ android/app/src/main/java/com/social/presentation/authentication/LoginFragment.kt
@@ -70,11 +70,11 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     }
 
     private fun googleLoginButton() {
-        showMessage(requireContext(), "Falta 10")
+        showMessage(requireContext(), "Pendiente 1")
     }
 
     private fun mastodonLoginButton() {
-        showMessage(requireContext(), "Falta 7")
+        showMessage(requireContext(), "Pendiente 2")
     }
 
     private fun setupEmailValidation() {
