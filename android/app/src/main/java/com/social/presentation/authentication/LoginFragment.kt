ff946c4f7e635d2f20618e49223596e9204da361
serv10 <sebastian.ramos.villanueva10@gmail.com>
2023-12-01 19:09:59 -0500

    hola

--- android/app/src/main/java/com/social/presentation/authentication/LoginFragment.kt
+++ android/app/src/main/java/com/social/presentation/authentication/LoginFragment.kt
@@ -95,7 +95,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     }
 
     private fun googleLoginButton() {
-        showMessage(requireContext(), "Pendiente 1")
+        showMessage(requireContext(), "Pendiente 2")
     }
 
     private fun mastodonLoginButton() {
